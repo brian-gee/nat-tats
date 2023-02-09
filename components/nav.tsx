fd2ccb9b48304f2e47f691754bfe2301326d100b
Brian Geertsma <briange@tuta.io>
2023-02-09 15:58:22 -0500

    adding nav

---
+++ components/nav.tsx
@@ -0,0 +1,59 @@
+import { Inter } from '@next/font/google';
+import { HiMenuAlt1 } from 'react-icons/hi';
+import { BsInstagram } from 'react-icons/bs';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Nav() {
+  const navItems = [
+    {
+      name: 'Home',
+      href: '#home',
+    },
+    {
+      name: 'Services',
+      href: '#services',
+    },
+    {
+      name: "FAQ's",
+      href: '#faqs',
+    },
+    {
+      name: 'Contact',
+      href: '#contact',
+    },
+    {
+      name: <BsInstagram />,
+      href: '#socials',
+    },
+  ];
+
+  return (
+    <main>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">NAT TATS</a>
+        </div>
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <div className="dropdown">
+              <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                <HiMenuAlt1 className="text-xl" />
+              </label>
+              <ul
+                tabIndex={0}
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                {navItems.map((item, i) => (
+                  <li key={i}>
+                    <a href={item.href}>{item.name}</a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="dropdown dropdown-end"></div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import Header from '@/components/header';
+import Nav from '@/components/nav';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Header />
+      <Nav />
       <Component {...pageProps} />
     </div>
   );
