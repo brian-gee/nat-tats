72e75747b070388fc70e70f02f62bfc85555cf64
Brian Geertsma <briange@tuta.io>
2023-02-08 23:59:48 -0500

    removing extras

---
+++ components/header.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+
+export default function Header({}) {
+  return (
+    <main>
+      <Head>
+        <title>NAT TATS</title>
+        <meta name="description" content="Natalia Rezende Tattoo Artist" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </main>
+  );
+}
